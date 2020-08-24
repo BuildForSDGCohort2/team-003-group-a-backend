9433d98c90871d4c922c87ab9af7f9a63d14d1f0
SamuelALSN <samuelgwfirst@gmail.com>
2020-08-24 15:47:24 +0000

    added procfile for heroku deployment

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const http = require('http');
+const port = process.env.APP_PORT || 3000 ;
 
 const server = http.createServer((req,res)=> {
      res.end('Welcome to the Educonnecte  API');
 })
-server.listen(process.env.APP_PORT || 3000);
+server.listen(port);

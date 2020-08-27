b457ef3f1aabb6c2562f0d3f517e82847dfc32d6
Duncanwar <semunda9@gmail.com>
2020-08-27 17:53:38 +0200

    set up project

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-//require('dotenv').config();
-const PORT = process.env.PORT || 5000 ;
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
 
 const http = require('http');
-const app  = require('./app');
+const app = require('./app');
 
-app.set('port',PORT);
+app.set('port', PORT);
 const server = http.createServer(app);
 
 server.listen(PORT);
